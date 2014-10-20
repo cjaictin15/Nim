298db948d81c9a788ae2c9abbf4cc65b4fbfeac4
cjaictin15 <cjaictin15@14ac02mtqfhg083.punahou.edu>
2014-10-20 13:09:06 -1000

    CJ's Smart Computer Nim Player
    Wins a game of Nim 67% of the time

--- RandomNim.java
+++ RandomNim.java
@@ -1,27 +1,13 @@
-import java.util.Scanner;
-
 public class RandomNim {
-  private int pieces;
-  
-  Scanner in = new Scanner(System.in);
-
-  
-  public int play(int pieces){
-    pieces = pieces % 3;
-    
-    if (pieces == 2) {
-      pieces = pieces - 1;
-      return pieces;
+  public int play(int pieces){ 
+    if ((pieces % 3 == 2) || (pieces % 3 == 1)) { // if the pieces left leaves a remainder of 2 or 1 when divided by three...
+      System.out.println("Computer took 1 piece");
+      return 1; // computer returns 1
     }
     
-    if (pieces == 1) {
-      pieces = pieces - 1;
-      return pieces;
-    }
-    
-    if (pieces == 0); {
-      pieces = pieces - 2;
-      return pieces;
+    if (pieces % 3 == 0); { // if the pieces left leaves a remainder of 2 or 1 when divided by three...
+      System.out.println("Computer took 2 pieces");
+      return 2; // computer returns 2
     } 
   }
 }
